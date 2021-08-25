631c065168177fce7ac38e733e2a0028e1ff4572
Josh <47988616+Bappy0X@users.noreply.github.com>
2021-08-25 17:55:29 +0100

    More changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,15 +14,15 @@ export default function Footer() {
     return (
         <Container className="fixed-bottom mb-3">
             <Row noGutters className="justify-content-center">
-                <MediaIcon name="GitHub" icon="fab fa-github"/>
+                <MediaIcon name="GitHub" icon="fab fa-github" url="https://github.com/Bappy0X"/>
                 <Divider/>
-                <MediaIcon name="LinkedIn" icon="fab fa-linkedin"/>
+                <MediaIcon name="LinkedIn" icon="fab fa-linkedin" url="https://www.linkedin.com/in/joshua-vaughan/"/>
                 <Divider/>
-                <MediaIcon name="Discord" icon="fab fa-discord"/>
+                <MediaIcon name="Discord" icon="fab fa-discord" url="https://discord.com/"/>
                 <Divider/>
-                <MediaIcon name="Dribbble" icon="fab fa-dribbble"/>
+                <MediaIcon name="Dribbble" icon="fab fa-dribbble" url="https://dribbble.com/Bappy0X"/>
                 <Divider/>
-                <MediaIcon name="YouTube" icon="fab fa-youtube"/>
+                <MediaIcon name="YouTube" icon="fab fa-youtube" url="https://www.youtube.com/channel/UCyAKeyc9I7gYRhzOV0ZadSg"/>
             </Row>
         </Container>
     )
--- src/components/mediaIcon.js
+++ src/components/mediaIcon.js
@@ -1,9 +1,9 @@
 import { Col } from "react-bootstrap";
 
-export default function MediaIcon({name, icon}) {
+export default function MediaIcon({name, icon, url}) {
     return (
         <Col xs="auto">
-            <a className="text-muted mb-0" href="" style={{textDecoration: "none"}}><i className={icon}></i> {name}</a>
+            <a className="text-muted mb-0" href={url} style={{textDecoration: "none"}}><i className={icon}></i> {name}</a>
         </Col>
     )
 }
