365e39d72b99992f6bf06deb26782e9867c632c1
Josh <47988616+Bappy0X@users.noreply.github.com>
2021-11-23 12:45:35 +0000

    Promote to Production (#1)
    * Light ver
    
    * Light ver
    
    * Brand new design init
    
    * Some quick updates
    
    * More changes
    
    * New card, link fixes
    
    * Link Changes

---
+++ src/components/mediaIcon.js
@@ -0,0 +1,9 @@
+import { Col } from "react-bootstrap";
+
+export default function MediaIcon({name, icon, url}) {
+    return (
+        <Col xs="auto">
+            <a className="text-muted mb-0" href={url} target="_blank" rel="noreferrer" style={{textDecoration: "none"}}><i className={icon}></i> {name}</a>
+        </Col>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
-import App from "./App";
+import Footer from "./components/footer"
+
+import signature from "./img/signature.svg"
+import card from "./img/card.png"
 
 ReactDOM.render(
-	<App/>,
+	<div id="true-content">
+		<Container>
+			<Row className="vh-100 align-content-center">
+				<Col className="text-center text-sm-right mb-3">
+					<div style={{transformStyle: "preserve-3d", transform: "perspective(400px) rotateX(0deg) rotateY(10deg) scale3d(1, 1, 1)"}}>
+						<img src={card} style={{height: "400px"}} alt="My Card"/>
+					</div>
+				</Col>
+				<Col className="text-center text-sm-left align-self-center">
+					<h1 className="accent primary">Joshua Vaughan</h1>
+					<p>Great to see you! I'm Josh - a developer from England. I have experience working with multiple technologies, I also love coffee and the gym.<br/>I'm also currently attended college; studying Computer Science, Business Studies and Public Services.</p>
+					<h4><a href="https://open.spotify.com/playlist/3Al07FeGVgigYo3IX9wuhO?si=dbfef3e582e54721" target="_blank" rel="noreferrer" className="primary" style={{textDecoration: "none"}}><i className="fab fa-spotify mr-2 text-success"></i>Gym Essentials Playlist</a></h4>
+					<img src={signature} className="primary" style={{height: "50px"}} alt="My Signature"/>
+				</Col>
+			</Row>
+		</Container>
+		<Footer/>
+	</div>,
 	document.getElementById("content")
 );
