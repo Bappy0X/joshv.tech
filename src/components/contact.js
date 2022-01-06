937eae6f31e17daf795cf9e15e5898737f6e80d7
Josh <47988616+Bappy0X@users.noreply.github.com>
2022-01-06 19:46:45 +0000

    New Email

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ export default function Contact() {
             <Row noGutters className="justify-content-center justify-content-md-start">
                 <Icon name="Call" icon="fas fa-phone" url="tel:07518387243"/>
                 <Divider/>
-                <Icon name="Email" icon="fas fa-envelope" url="mailto:josh@bappy0x.me"/>
+                <Icon name="Email" icon="fas fa-envelope" url="mailto:me@joshv.tech"/>
             </Row>
         </div>
     )
