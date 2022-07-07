7116d2c857892e1f541518a30f4437575eb0e991
Josh <47988616+Bappy0X@users.noreply.github.com>
2022-07-07 22:56:12 +0100

    New updates + changes

--- src/components/Divider.jsx
+++
@@ -1,9 +0,0 @@
-import { Col } from "react-bootstrap"
-
-export default function Divider() {
-    return (
-        <Col xs="auto" className="mx-3">
-            <div className="divider"/>
-        </Col>
-    )
-}
--- src/components/Icon.jsx
+++
@@ -1,9 +0,0 @@
-import { Col } from "react-bootstrap";
-
-export default function Icon({name, icon, url, colour="primary", fullWidth=false}) {
-    return (
-        <Col xs={fullWidth ? "6" :"auto"}>
-            <a className={colour + " text-decoration-none"} href={url} target="_blank" rel="noreferrer"><i className={icon + " mr-1"}></i>{name}</a>
-        </Col>
-    )
-}
