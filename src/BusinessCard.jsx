fea4efb29c1d224d7562f3cc8d2b72ef8b9a280f
Josh <47988616+joshvtech@users.noreply.github.com>
2022-07-17 15:13:49 +0100

    Add titles, etc.

--- src/BusinessCard.jsx
+++ src/BusinessCard.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import { Row, Col } from "react-bootstrap"
 
 import { MouseContext } from "./Layout"
@@ -10,11 +11,16 @@ export default class BusinessCard extends React.Component {
 
     render() {
         return(
-            <Row className="no-gutters my-3" onMouseMove={this.onMouseMove}>
-                <Col xs="12" className="text-center">
-                    <img src={card} alt="My Business Card" style={{height: "500px", transformStyle: "preserve-3d", transform: `perspective(500px) rotateX(${this.context.rotX}deg) rotateY(${this.context.rotY}deg) scale3d(1, 1, 1)`}}/>
-                </Col>
-            </Row>
+            <>
+                <Helmet>
+                    <title>Business Card &bull; joshv.tech</title>
+                </Helmet>
+                <Row className="no-gutters my-3" onMouseMove={this.onMouseMove}>
+                    <Col xs="12" className="text-center">
+                        <img src={card} alt="My Business Card" style={{height: "500px", transformStyle: "preserve-3d", transform: `perspective(500px) rotateX(${this.context.rotX}deg) rotateY(${this.context.rotY}deg) scale3d(1, 1, 1)`}}/>
+                    </Col>
+                </Row>
+            </>
         )
     }
 }
