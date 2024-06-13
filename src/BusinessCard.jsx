707b416669f2659c85df069f334372bbb79e63cc
Josh <47988616+joshvtech@users.noreply.github.com>
2024-06-13 02:22:36 +0100

    Revamp

---
+++ src/BusinessCard.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { Row, Col } from "react-bootstrap"
+
+import { MouseContext } from "./Layout"
+
+import card from "./img/card.jpg";
+import logo from "./img/logo.png";
+import { Link } from "react-router-dom";
+
+class ContactLink extends React.Component {
+    render() {
+        return(
+            <h1><i className={`fas fa-${this.props.icon} fa-fw text-primary`}/> <a href={this.props.url} className="text-muted text-decoration-none" target="_blank" rel="noreferrer">{this.props.text}</a></h1>
+        )
+    }
+}
+
+export default class BusinessCard extends React.Component {
+    static contextType = MouseContext
+
+    render() {
+        return(
+            <>
+                <Helmet>
+                    <title>Home &bull; joshv.tech</title>
+                </Helmet>
+                <Row className="no-gutters my-3" onMouseMove={this.onMouseMove}>
+                    <Col xs="4" className="ml-auto text-center">
+                        <img src={card} alt="My Business Card" style={{height: "500px", transformStyle: "preserve-3d", transform: `perspective(500px) rotateX(${this.context.rotX}deg) rotateY(${this.context.rotY}deg) scale3d(1, 1, 1)`}}/>
+                    </Col>
+                    <Col xs="3" className="my-auto mr-auto text-left ">
+                        <img src={logo} alt="My Logo" style={{height: "150px"}}/>
+                        <hr className="my-4"/>
+                        <p className="text-primary">Your partner in Websites, Software, Teams, AI or anything tech!</p>
+                        <ContactLink icon="envelope" url="mailto:me@joshv.tech" text="me@joshv.tech"/>
+                        <ContactLink icon="phone-alt" url="tel:+44 7518 387243" text="+44 7518 387243"/>
+                        <h3 className="mt-4"><Link to="/my-portfolio" className="text-primary text-decoration-none">View My Portfolio <i className="fas fa-arrow-right text-primary"></i></Link></h3>
+                    </Col>
+                </Row>
+            </>
+        )
+    }
+}
---
+++ src/Portfolio.jsx
@@ -0,0 +1,78 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { Container, Row, Col } from "react-bootstrap"
+import { Fade } from "react-reveal"
+import { Link } from "react-router-dom"
+
+import logo from "./img/logo.png";
+
+import PennySkips from "./img/PennySkips.jpg"
+import Locked from "./img/Locked.png"
+
+import Barbertini from "./img/Barbertini.jpg"
+import GaultonFitness from "./img/GaultonFitness.jpg"
+import Fireless from "./img/Fireless.jpg"
+import RAFAC from "./img/RAFAC.jpg"
+import HollySumnerTherapy from "./img/HollySumnerTherapy.jpg"
+import FBALeadsUK from "./img/FBALeadsUK.jpg"
+
+class ProjectCard extends React.Component {
+    render() {
+        return(
+            <Col xs={12} md={6} lg={4} className="my-3">
+                <Fade left delay={this.props.fade}>
+                    <div className="text-center">
+                        <a href={this.props.url} target="_blank" rel="noreferrer">
+                            <img src={this.props.img} alt="Portfolio" className="w-75 rounded-lg"/>
+                        </a>
+                        <Row className={`no-gutters py-3 ${this.props.overlap && "mt--3"} px-3 bg-light rounded-xl position-relative`}>
+                            <Col>
+                                <h4 className="mb-0 font-weight-bold">{this.props.name}</h4>
+                                { this.props.url ? <a href={this.props.url} target="_blank" rel="noreferrer" className="text-decoration-none text-primary">{this.props.url} <i className="fas fa-external-link-alt"/></a> : <p className="m-0 text-muted">Coming Soon...</p> }
+                            </Col>
+                        </Row>
+                    </div>
+                </Fade>
+            </Col>
+        )
+    }
+}
+
+export default class Portfolio extends React.Component {
+    render() {
+        return(
+            <>
+                <Helmet>
+                    <title>My Portfolio &bull; joshv.tech</title>
+                </Helmet>
+                <Container>
+                    <Row className="mt-3">
+                        <Col className="my-auto">
+                            <h4 className="my-4"><Link to="/" className="text-muted text-decoration-none"><i className="fas fa-arrow-left text-muted"></i> Back to Home</Link></h4>    
+                        </Col>
+                        <Col className="col-auto">
+                        <Link to="/"><img src={logo} alt="My Logo" style={{height: "80px"}}/></Link>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                    
+                    <Row>
+                        <ProjectCard img={PennySkips} name="PennySkips" url="https://pennyskips.com/" overlap fade={150*0}/>
+                        <ProjectCard img={Locked} name="CountyStream" overlap fade={150*0}/>
+                        <ProjectCard img={Locked} name="???" overlap fade={150*0}/>
+                    </Row>
+                    <hr className="my-4"/>
+                    <Row>
+                        <ProjectCard img={Barbertini} name="Barbertini" url="https://barbertini.co.uk/" overlap fade={150*0}/>
+                        <ProjectCard img={GaultonFitness} name="Gaulton Fitness" url="https://gaultonfitness.co.uk/" overlap fade={150*1}/>
+                        <ProjectCard img={Fireless} name="Fireless Ltd" url="https://230rafac.co.uk/" overlap fade={150*2}/>
+                        <ProjectCard img={RAFAC} name="Royal Air Force Air Cadets" url="https://230rafac.co.uk/" overlap fade={150*3}/>
+                        <ProjectCard img={HollySumnerTherapy} name="Holly Sumner Therapy" url="https://hollysumnertherapy.com/" overlap fade={150*4}/>
+                        <ProjectCard img={FBALeadsUK} name="FBALeadsUK" url="https://fbaleadsuk.com/" overlap fade={150*5}/>
+                    </Row>
+                    <hr className="my-4"/>
+                </Container>
+            </>
+        )
+    }
+}
